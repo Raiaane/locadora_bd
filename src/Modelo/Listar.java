36368b65875f4a40c7e71de42b146904cd8eaa49
Raiaane <43380217+Raiaane@users.noreply.github.com>
2018-11-15 20:51:39 -0200

    Listar pronto

--- src/Modelo/Listar.java
+++ src/Modelo/Listar.java
@@ -10,5 +10,34 @@ package Modelo;
  * @author Raiane
  */
 public class Listar {
-    
+static private int coddvd;
+static private int codealuguel;
+static private int codcliente; 
+
+    public static int getCoddvd() {
+        return coddvd;
+    }
+
+    public static void setCoddvd(int coddvd) {
+        Listar.coddvd = coddvd;
+    }
+
+    public static int getCodealuguel() {
+        return codealuguel;
+    }
+
+    public static void setCodealuguel(int codealuguel) {
+        Listar.codealuguel = codealuguel;
+    }
+
+    public static int getCodcliente() {
+        return codcliente;
+    }
+
+    public static void setCodcliente(int codcliente) {
+        Listar.codcliente = codcliente;
+    }
+
+
+
 }
